b33db1d3070c6c0a2b89ac31073ea06114804176
MinjaeLee <2alswo7@khu.ac.kr>
2021-11-13 15:39:19 +0900

    ✨[FEAT] - 네트워크 샘플 추가

---
+++ Study_iOS/Study_iOS/Sources/Network/APIConstants.swift
@@ -0,0 +1,14 @@
+//
+//  APIConstants.swift
+//  Study_iOS
+//
+//  Created by 민 on 2021/11/13.
+//
+
+import Foundation
+struct APIConstants {
+    static let baseURL = "http://cherishserver.com"
+    
+    static let loginURL = "http://cherishserver.com/login/signin"
+    static let signupURL = "http://cherishserver.com/login/signup"
+}
