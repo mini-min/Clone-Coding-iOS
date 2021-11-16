6062d917e5b0f9b355cfa54fdb34f1b1e64e05f7
MinjaeLee <2alswo7@khu.ac.kr>
2021-11-16 15:02:11 +0900

    ✨[FEAT] - ImageLiteral, ColorLiteral 연습 & 적용

--- Study_iOS/Study_iOS/Sources/SnapKit/ExampleSnapkitViewController.swift
+++ Study_iOS/Study_iOS/Sources/SnapKit/ExampleSnapkitViewController.swift
@@ -6,24 +6,58 @@
 //
 
 import UIKit
+import SnapKit
 
 class ExampleSnapkitViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    let tempColor = UIColor(red: 201/255, green: 48/255, blue: 52/255, alpha: 1.0)
+    let tempImage = UIImage(named: "homeBtnOnClick")
+    
+    let literalColor: UIColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+    let literalImage: UIImage = #imageLiteral(resourceName: "homeBtnOnClick")
+    
+    // 흐린 화면 뷰
+    private let exampleView: UIView = {
+        let view = UIView()
+        view.backgroundColor = #colorLiteral(red: 0.4445274472, green: 0.4969258904, blue: 0.9510256648, alpha: 1)
+        return view
+    }()
+        
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .white
+        setupUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - @IBAction Properties
+    
+    // MARK: - @Functions
+    // UI 세팅 작업
+    private func setupUI() {
+        view.addSubview(exampleView)
+        
+        setupLayout()
     }
-    */
-
+    
+    // 레이아웃 세팅
+    private func setupLayout() {
+        //        exampleView.translatesAutoresizingMaskIntoConstraints = false
+        //        NSLayoutConstraint.activate([
+        //            exampleView.topAnchor.constraint(equalTo: view.topAnchor),
+        //            exampleView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+        //            exampleView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        //            exampleView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        //        ])
+        
+        exampleView.snp.makeConstraints { (make) -> Void in
+            make.top.equalToSuperview().inset(additionalSafeAreaInsets.top + 20)
+            make.left.equalToSuperview().inset(20)
+            make.bottom.equalToSuperview().inset(20)
+            make.right.equalToSuperview().inset(20)
+        }
+    }
 }
