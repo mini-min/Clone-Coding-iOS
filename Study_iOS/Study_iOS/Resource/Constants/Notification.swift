69a8893c85c3eada33c6a959050ccf1be0038867
MinjaeLee <2alswo7@khu.ac.kr>
2022-01-20 14:51:21 +0900

    [REMOVE] 모든 파일 재세팅을 위해 파일 삭제

--- Study_iOS/Study_iOS/Resource/Constants/Notification.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Notification.swift
-//  Study_iOS
-//
-//  Created by 민 on 2021/12/08.
-//
-
-import Foundation
--- Study_iOS/Study_iOS/Resource/Constants/UserDefaults.swift
+++
@@ -1,8 +0,0 @@
-//
-//  UserDefaults.swift
-//  Study_iOS
-//
-//  Created by 민 on 2021/12/08.
-//
-
-import Foundation
