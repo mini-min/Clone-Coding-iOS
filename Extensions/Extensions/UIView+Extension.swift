30e0424d47eaefdaca18db26a7711841af264082
MinjaeLee <2alswo7@khu.ac.kr>
2022-01-20 15:29:56 +0900

    [FEAT] #2 - UIView, UIAlertController, UITextField Extension 코드 추가

---
+++ Extensions/Extensions/UIView+Extension.swift
@@ -0,0 +1,52 @@
+//
+//  UIView+Extension.swift
+//  Extensions
+//
+//  Created by 민 on 2022/01/20.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - 스토리보드에서 실시간으로 편리하게 랜더링 하기 위한 Extension
+
+@IBDesignable
+extension UIView {
+    
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+}
