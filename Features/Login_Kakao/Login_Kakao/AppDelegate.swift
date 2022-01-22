6291a658836a2b90f57a26da0b2b2e5287362194
MinjaeLee <2alswo7@khu.ac.kr>
2022-01-22 20:30:56 +0900

    [FEAT] #9 - Features - Update Kakao Login Part

---
+++ Features/Login_Kakao/Login_Kakao/AppDelegate.swift
@@ -0,0 +1,36 @@
+//
+//  AppDelegate.swift
+//  Login_Kakao
+//
+//  Created by 민 on 2022/01/22.
+//
+
+import UIKit
+import KakaoSDKCommon
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        KakaoSDKCommon.initSDK(appKey: "51c56dada90f709d5a008cbf2560f239")
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
