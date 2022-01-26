cc00b652cadddab318529f0dd27dde631e218cbb
MinjaeLee <2alswo7@khu.ac.kr>
2022-01-26 12:06:13 +0900

    [FEAT] #13 - CoreML: Update ObjectRecognition Project

---
+++ CoreML/1_ObjectRecognition/1_ObjectRecognition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1_ObjectRecognition
+//
+//  Created by 민 on 2022/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
