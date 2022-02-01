4cd38c68cca9643e8351bc9c7d0176b2db26681a
MinjaeLee <2alswo7@khu.ac.kr>
2022-02-01 21:38:18 +0900

    [FEAT] #21 - Update UIKit Project

---
+++ UIKit/UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit
+//
+//  Created by 민 on 2022/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
