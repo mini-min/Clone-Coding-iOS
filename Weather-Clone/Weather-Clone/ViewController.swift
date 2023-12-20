36c0a7ca0578a4ef635394f2a99ee90ed181a29f
MinjaeLee <2alswo7@khu.ac.kr>
2023-12-20 18:14:10 +0900

    레포 초기 세팅

---
+++ Weather-Clone/Weather-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather-Clone
+//
+//  Created by 민 on 12/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
